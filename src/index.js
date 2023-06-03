3e1c403d6c65d73e60912b09c6366a9bd0f89a14
cjranjana <98210105+cjranjana@users.noreply.github.com>
2023-06-03 08:00:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,47 +18,6 @@ root.render(
   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
 
-// import reducer, { initialState } from './reducer';
-// import { StateProvider } from './StateProvider';
-
-// class ErrorBoundary extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { hasError: false };
-//   }
-
-//   static getDerivedStateFromError(error) {
-//     // Update state so the next render will show the fallback UI.
-//     return { hasError: true };
-//   }
-
-//   render() {
-//     if (this.state.hasError) {
-//       // You can render any custom fallback UI
-//       return <h1>Something went wrong.</h1>;
-//     }
-
-//     return this.props.children;
-//   }
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <StateProvider initialState={initialState} reducer={reducer}>
-//       <ErrorBoundary>
-//         <App />
-//       </ErrorBoundary>
-//     </StateProvider>
-//   </React.StrictMode>,
-// );
 
 
